72cfb8832cda9a2171744cf81e15b2cbc08e309c
gleriston.santana <gleriston.santana@NBTITMA149293.tivit.corp>
2021-12-29 15:33:07 -0300

    Inclusão de funções de segurança na aplicação.

--- eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -2,12 +2,16 @@ package com.eventoapp.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class EventoappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventoappApplication.class, args);
+	    System.out.print(new BCryptPasswordEncoder().encode("456"));
 	}
 
 }
